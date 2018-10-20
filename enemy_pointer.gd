e7b19b4dfc895b05b0597e8638310cbc2f032124
Plopsis <35765659+Plopsiskopsis@users.noreply.github.com>
2018-10-20 09:01:57 +0300

    fix for pointer

--- enemy_pointer.gd
+++ enemy_pointer.gd
@@ -6,9 +6,8 @@ func _process(delta):
 	var group = get_tree().get_nodes_in_group("enemy")
 	for i in group:
 		if closest != null:
-			if translation.distance_to(i.translation) < translation.distance_to(closest.translation):
+			if get_parent().translation.distance_to(i.translation) < get_parent().translation.distance_to(closest.translation):
 				closest = i
-				print(closest)
 		else:
 			closest = i
 	look_at(closest.translation, Vector3(0.0, 1.0, 0.0))
