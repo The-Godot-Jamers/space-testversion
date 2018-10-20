244e028e668dc4118be07a93d4baeebab4820bd5
Plopsis <35765659+Plopsiskopsis@users.noreply.github.com>
2018-10-20 10:26:41 +0300

    pointer fix for near

--- enemy_pointer.gd
+++ enemy_pointer.gd
@@ -10,5 +10,9 @@ func _process(delta):
 				closest = i
 		else:
 			closest = i
+	if get_parent().translation.distance_to(closest.translation) < 15.0:
+		visible = false
+	else:
+		visible = true
 	look_at(closest.translation, Vector3(0.0, 1.0, 0.0))
 	closest = null
--- hit_progress.gd
+++ hit_progress.gd
@@ -13,6 +13,11 @@ func progress(amt):
 		$reset_timer.start(0.5)
 		visible = true
 		value += amt
+	if value >= max_value:
+		#here change scene and takeout the print
+		print("reached max on progress bar") 
+		visible = false
+		value = 0
 
 func _on_reset_timer_timeout():
 	visible = false
