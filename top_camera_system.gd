caf6bb183936ef57f422f083d1ad633dba148270
Plopsis <35765659+Plopsiskopsis@users.noreply.github.com>
2018-10-22 21:11:23 +0300

    new camera

---
+++ top_camera_system.gd
@@ -0,0 +1,18 @@
+extends Position3D
+
+
+export var zoom_value = 0.5
+
+
+func _input(event):
+	if event.is_action_pressed("zoomin"):
+		$Tween.interpolate_property(self,"translation", translation, Vector3(translation.x, translation.y - zoom_value, translation.z), 0.1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT) 
+		$Tween.start()
+	elif event.is_action_pressed("zoomout"):
+		$Tween.interpolate_property(self,"translation", translation, Vector3(translation.x, translation.y + zoom_value, translation.z), 0.1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT) 
+		$Tween.start()
+
+func _process(delta):
+	rotation.y = -get_parent().rotation.y
+
+
