a0cf7165695ac0fd0b0f9c85ba62250ff27c5527
Plopsis <35765659+Plopsiskopsis@users.noreply.github.com>
2019-01-13 12:01:32 +0200

    basic AI movement for robo

--- robot_1.gd
+++ robot_1.gd
@@ -2,17 +2,17 @@ extends KinematicBody
 
 var piupiu = preload("res://piupiu_laser.tscn")
 var target
-var move_speed = 50
+var move_speed = 5
 var velocity = Vector3()
 var accel = 3
-var test = 0
+var rot_speed = 2
 
 func _ready():
 	target = globals.player
 
 func _physics_process(delta):
-	$Tween.interpolate_property(self, "transform",transform,transform.looking_at(target.translation,Vector3(0,1,0)),0.2,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
-	$Tween.start()
+	
+	look_at(target.translation,Vector3(0,1,0)) #need to replace with something not instant.
 	
 	if translation.distance_to(target.translation) > 5:
 		var direction = Vector3()
@@ -21,21 +21,17 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		var target_move = direction * move_speed
 		velocity = velocity.linear_interpolate(target_move,accel * delta)
-		test += 1
-		if test > 100:
-			print(translation.distance_to(target.translation))
-			print(velocity)
-			test = 0
 		velocity.y = 0
 		velocity = move_and_slide(velocity)
 
 func _on_laser_timer_timeout():
-	var laser = piupiu.instance()
-	laser.translation = translation
-	laser.shooter = self
-	laser.rotation.y = rotation.y - deg2rad(180)
-	get_parent().add_child(laser)
-	$AnimationPlayer.play("Shot")
+	if translation.distance_to(target.translation) < 20:
+		var laser = piupiu.instance()
+		laser.translation = translation
+		laser.shooter = self
+		laser.rotation.y = rotation.y - deg2rad(180)
+		get_parent().add_child(laser)
+		$AnimationPlayer.play("Shot")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	$AnimationPlayer.play("Follow")
